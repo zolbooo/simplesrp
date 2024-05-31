c1231147567c359ffe748e3d70d6a8c9437f2ce6
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-05-31 23:53:02 +0800

    refactor: move test vector data to the separate file

--- test/verifier.spec.ts
+++ test/verifier.spec.ts
@@ -3,23 +3,7 @@ import { expect, test } from "vitest";
 import { deriveVerifier } from "../src/srp/verifier";
 import { byteArrayToHexString, hexStringToByteArray } from "../src/utils";
 
-// See: https://datatracker.ietf.org/doc/html/rfc5054#appendix-A
-const N_1024 = `
-  EEAF0AB9 ADB38DD6 9C33F80A FA8FC5E8 60726187 75FF3C0B 9EA2314C
-  9C256576 D674DF74 96EA81D3 383B4813 D692C6E0 E0D5D8E2 50B98BE4
-  8E495C1D 6089DAD1 5DC7D7B4 6154D6B6 CE8EF4AD 69B15D49 82559B29
-  7BCF1885 C529F566 660E57EC 68EDBC3C 05726CC0 2FD4CBF4 976EAA9A
-  FD5138FE 8376435B 9FC61D2F C0EB06E3
-`.replace(/\s/g, "");
-const I = "alice";
-const s = "BEB25379 D1A8581E B5A72767 3A2441EE".replace(/\s/g, "");
-const v = `
-  7E273DE8 696FFC4F 4E337D05 B4B375BE B0DDE156 9E8FA00A 9886D812
-  9BADA1F1 822223CA 1A605B53 0E379BA4 729FDC59 F105B478 7E5186F5
-  C671085A 1447B52A 48CF1970 B4FB6F84 00BBF4CE BFBB1681 52E08AB5
-  EA53D15C 1AFF87B2 B9DA6E04 E058AD51 CC72BFC9 033B564E 26480D78
-  E955A5E2 9E7AB245 DB2BE315 E2099AFB
-`.replace(/\s/g, "");
+import { I, N_1024, s, v } from "./test-vector-rfc5054";
 
 test("it should derive verifier according as per RFC5054", async () => {
   const { verifier } = await deriveVerifier("password123", {
