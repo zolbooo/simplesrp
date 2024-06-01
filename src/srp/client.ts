0be485f3fdcc48338c9e2dfd2c305ef7c711084a
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-06-01 12:01:35 +0800

    feat(srp): client-side session key derivation

--- src/srp/client.ts
+++ src/srp/client.ts
@@ -1,6 +1,15 @@
 import { G, N } from "../constants";
 import { modPow } from "../math";
-import { generateRandomExponent, hexStringToByteArray } from "../utils";
+import {
+  bigIntToByteArray,
+  byteArrayToBigInt,
+  hexStringToByteArray,
+  generateRandomExponent,
+} from "../utils";
+
+import { deriveSharedHash } from "./common";
+import { DigestFn, deriveVerifier, digestPBKDF2 } from "./verifier";
+import { DeriveMultiplierFn, deriveMultiplierSRP6a } from "./multiplier";
 
 export function generateClientEphemeral({
   N: modulo = N,
@@ -32,4 +41,65 @@ export function generateClientEphemeral({
   }
 }
 
+function modNegative(a: bigint, n: bigint): bigint {
+  while (a < 0n) {
+    a += n;
+  }
+  return a % n;
+}
+export async function deriveSessionKey({
+  password,
+  salt,
+  clientPrivateEphemeral,
+  clientPublicEphemeral,
+  serverPublicEphemeral,
+  sharedHash,
+  deriveMultiplier = deriveMultiplierSRP6a,
+  digest = digestPBKDF2,
+  N: modulo = N,
+  G: generator = G,
+}: {
+  password: string;
+  salt: Uint8Array;
+  clientPrivateEphemeral: Uint8Array;
+  clientPublicEphemeral: Uint8Array;
+  serverPublicEphemeral: Uint8Array;
+  sharedHash?: Uint8Array;
+  deriveMultiplier?: DeriveMultiplierFn;
+  digest?: DigestFn;
+  N?: bigint;
+  G?: bigint;
+}): Promise<Uint8Array> {
+  const u = byteArrayToBigInt(
+    sharedHash ??
+      (await deriveSharedHash({
+        clientPublicEphemeral,
+        serverPublicEphemeral,
+        N: modulo,
+      }))
+  );
+  const k = byteArrayToBigInt(
+    await deriveMultiplier(
+      bigIntToByteArray(modulo),
+      bigIntToByteArray(generator)
+    )
+  );
+  const { x: xBytes } = await deriveVerifier(password, {
+    salt,
+    N: modulo,
+    G: generator,
+    digest,
+  });
+  const x = byteArrayToBigInt(xBytes);
+  // B - k * g^x
+  const base = modNegative(
+    byteArrayToBigInt(serverPublicEphemeral) -
+      ((k * modPow(generator, x, modulo)) % modulo),
+    modulo
+  );
+  const exp = (byteArrayToBigInt(clientPrivateEphemeral) + u * x) % modulo;
+  return bigIntToByteArray(modPow(base, exp, modulo));
+}
+
 export * from "./verifier";
+export * from "./multiplier";
--- test/client.spec.ts
+++ test/client.spec.ts
@@ -1,7 +1,26 @@
 import { expect, test } from "vitest";
 
-import { byteArrayToBigInt } from "../src/utils";
-import { generateClientEphemeral } from "../src/srp/client";
+import {
+  byteArrayToBigInt,
+  byteArrayToHexString,
+  hexStringToByteArray,
+} from "../src/utils";
+import {
+  deriveSessionKey,
+  generateClientEphemeral,
+  deriveMultiplierSRP6a_SHA1,
+} from "../src/srp/client";
+
+import {
+  a,
+  A,
+  B,
+  s,
+  u,
+  N_1024,
+  premaster,
+  testDigestRFC5054,
+} from "./test-vector-rfc5054";
 
 test("it should generate a proper client ephemeral value", () => {
   const { clientPrivateEphemeral, clientPublicEphemeral } =
@@ -17,4 +36,21 @@ test("it should (almost) never generate the same client ephemeral value", () => 
   const { clientPublicEphemeral: clientPublicEphemeral2 } =
     generateClientEphemeral();
   expect(clientPublicEphemeral1).not.toEqual(clientPublicEphemeral2);
+});
+
+test("it should derive premaster secret correctly as per RFC5054", async () => {
+  const premasterSecret = await deriveSessionKey({
+    clientPrivateEphemeral: hexStringToByteArray(a),
+    clientPublicEphemeral: hexStringToByteArray(A),
+    serverPublicEphemeral: hexStringToByteArray(B),
+    sharedHash: hexStringToByteArray(u),
+    salt: hexStringToByteArray(s),
+    password: "password123",
+    N: BigInt("0x" + N_1024),
+    deriveMultiplier: deriveMultiplierSRP6a_SHA1,
+    digest: testDigestRFC5054,
+  });
+  expect(byteArrayToHexString(premasterSecret).toLowerCase()).toBe(
+    premaster.toLowerCase()
+  );
 });
