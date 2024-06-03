dac89b7789e1886237811bf9f6985277b33b7c6a
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-06-03 13:54:00 +0800

    test: add sanity tests for security

---
+++ test/security.spec.ts
@@ -0,0 +1,65 @@
+import { expect, test } from "vitest";
+
+import { ClientSession, ServerSession } from "../src";
+
+test("it should reject handshake if username has changed", async () => {
+  const username = "alice";
+  const password = "password";
+  const { salt, verifier } = await ClientSession.deriveVerifier({
+    username,
+    password,
+  });
+  const clientSession = new ClientSession();
+  const { clientPublicEphemeral } = clientSession.initializeHandshake();
+  const serverSession = new ServerSession();
+  const { serverPublicEphemeral } = await serverSession.prepareHandshake({
+    salt,
+    verifier,
+    clientPublicEphemeral,
+  });
+  const { clientProof } = await clientSession.finalizeHandshake({
+    username: "eve",
+    password,
+    salt,
+    serverPublicEphemeral,
+  });
+  const { clientVerified, serverProof } = await serverSession.finalizeHandshake(
+    {
+      username,
+      clientProof,
+    }
+  );
+  expect(clientVerified).toBe(false);
+  expect(serverProof).toBe(null);
+});
+
+test("it should reject handshake if password was incorrect", async () => {
+  const username = "alice";
+  const password = "password";
+  const { salt, verifier } = await ClientSession.deriveVerifier({
+    username,
+    password,
+  });
+  const clientSession = new ClientSession();
+  const { clientPublicEphemeral } = clientSession.initializeHandshake();
+  const serverSession = new ServerSession();
+  const { serverPublicEphemeral } = await serverSession.prepareHandshake({
+    salt,
+    verifier,
+    clientPublicEphemeral,
+  });
+  const { clientProof } = await clientSession.finalizeHandshake({
+    username,
+    password: "incorrect",
+    salt,
+    serverPublicEphemeral,
+  });
+  const { clientVerified, serverProof } = await serverSession.finalizeHandshake(
+    {
+      username,
+      clientProof,
+    }
+  );
+  expect(clientVerified).toBe(false);
+  expect(serverProof).toBe(null);
+});
